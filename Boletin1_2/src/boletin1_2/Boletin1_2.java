a8467ec5d9bdab55da3307bfe884e7d11fbe056e
fojomars <fojomars@victoria130.danielcastelao.org>
2018-10-09 09:31:51 +0200

    Ejercicio completado

---
+++ Boletin1_2/src/boletin1_2/Boletin1_2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_2;
+
+/**
+ *
+ * @author fojomars
+ */
+public class Boletin1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float lado=3;
+        System.out.println("El area del cuadrado es: " + lado*lado);
+    }
+    
+}
